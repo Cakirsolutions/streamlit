60e93402762b46722b5c404d1ccd46026c40caab
Ramazan55Cakir <149517466+Ramazan55Cakir@users.noreply.github.com>
2024-04-13 15:36:41 +0200

    Create page_config.py

---
+++ page_config.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PIL import Image
+
+def set_page_config():
+    icon_path = "animations/icon2.png"
+
+    st.set_page_config(
+        page_title="PMO Dashboard",
+        page_icon=icon_path,
+        layout="wide",
+        initial_sidebar_state='expanded'
+    )
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+
+
+
