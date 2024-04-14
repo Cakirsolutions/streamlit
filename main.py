14d3c4c2319a6f64c60acb369546f8e473bdcc8d
Ramazan55Cakir <149517466+Ramazan55Cakir@users.noreply.github.com>
2024-04-14 21:05:19 +0200

    test

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 from page_config import set_page_config
 
 set_page_config()
-st.markdown(f'<h2 style="color:#990000">I am the best ! J.M.D. .......</h2>', unsafe_allow_html=True)
+st.markdown(f'<h2 style="color:#990000">Hakan mal aq</h2>', unsafe_allow_html=True)
