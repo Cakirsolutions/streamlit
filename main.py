4738f071b10bd89a7bf2e5087fd9b380232e73ff
Cakirsolutions <167028876+Cakirsolutions@users.noreply.github.com>
2024-04-16 16:54:21 +0200

    new

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 from page_config import set_page_config
 
 set_page_config()
-st.markdown(f'<h2 style="color:#990000">Hakkan Ramazan</h2>', unsafe_allow_html=True)
+st.markdown(f'<h2 style="color:#990000">Hakkan Ramazan Streamlit</h2>', unsafe_allow_html=True)
